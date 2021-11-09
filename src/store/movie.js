937741145a32fd5de75f55987bff319c7b61c1bc
Sera Yunmi Kim <sera@nanoomenergy.com>
2021-11-09 17:01:18 +0900

    poster, info blur 처리

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
 export default {
     // module!
     namespaced:true,
     // data!
     state:()=>({
         movies: [],
-        // message: '',
-        // loading: false
+        message: '',
+        loading: false
     }),
     // computed!
     getters:{
@@ -29,10 +30,11 @@ export default {
     // 비동기로 동작을 한다.
     actions:{
         async searchMovies({ state, commit }, payload){
-            const {title, type, number, year} = payload
-            const OMDB_API_KEY = '7035c60c'
-      
-            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+          try{
+            const res = await _fetchMovie({
+                ...payload, 
+                page:1
+            })
             const { Search, totalResults} = res.data
             commit('updateState', {
               movies: _uniqBy(Search, 'imdbID')
@@ -46,10 +48,12 @@ export default {
             // 추가 요청!
             if(pageLength > 1){
               for(let page = 2; page <= pageLength; page += 1){
-                if(page > (number / 10)) break 
-                
-                const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-                const { Search } = res.data;
+                if(page > (payload.number / 10)) break 
+                const res = await _fetchMovie({
+                    ...payload, 
+                    page
+                })
+                const { Search } = res.data
                 commit('updateState', {
                   movies: [
                     ...state.movies,
@@ -57,6 +61,30 @@ export default {
                 })
               }
             }
+          }catch(message){
+            commit('updateState', {
+                movies: [],
+                message
+            })
+          }
         }
     }
+}
+
+// 다른 곳에서는 사용하지 않고 현재파일 내부에서만 사용한다는 의미를 부여하기 위해 _를 함수명 앞에 붙여준다.
+function _fetchMovie(payload) {
+    const { title, type, year, page} = payload
+    const OMDB_API_KEY = '7035c60c'
+    const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+    return new Promise((resolve, reject)=>{
+        axios.get(url)
+            .then(res => {
+                  resolve(res)
+            })
+            .catch(err => {
+                reject(err.message)
+            })
+
+    });
 }
